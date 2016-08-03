c0b5afb39f5008f6db1351adc0e21a651072bc12
balupabbi <Bhargav@Muralidhars-iMac.local>
2016-08-02 23:36:09 -0700

    machine learning package created
    just started labeling data read cvs

--- SparkBook1/build.sbt
+++ SparkBook1/build.sbt
@@ -6,5 +6,6 @@ scalaVersion := "2.11.5"
 
 libraryDependencies ++= Seq(
   "org.apache.spark"	%% "spark-core"   % "1.5.2",
-  "org.apache.spark"  %% "spark-mllib"  % "1.5.2")
+  "org.apache.spark"  %% "spark-mllib"  % "1.5.2",
+  "org.apache.spark" %% "spark-sql" % "1.5.2")
     
---
+++ SparkBook1/src/main/scala-2.11/MachineLearning/Accelerameter.scala
@@ -0,0 +1,20 @@
+package MachineLearning
+
+/**
+  * Created by Bhargav on 8/2/16.
+  */
+
+import org.apache.spark.mllib.linalg._
+
+class Accelerameter {
+
+}
+
+
+object test1{
+  def main(args: Array[String]): Unit = {
+    val denseVector = Vectors.dense(1.0, 0.0, 3.0)
+    println(denseVector)
+
+  }
+}
